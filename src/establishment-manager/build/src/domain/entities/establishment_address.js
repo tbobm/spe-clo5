0196909e8ca4dba9af4ab651fa58c1692fa680d0
simoes_t <simoes_t@etna-alternance.net>
2020-07-02 10:44:02 +0200

    establishment, address

--- src/establishment-manager/build/src/domain/entities/establishment_address.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { attributes } = require("structure");
-const DomainEstablishmentAddress = attributes({
-    establishment_id: Number,
-    address_id: Number,
-})(class DomainEstablishmentAddress {
-});
-exports.DomainEstablishmentAddress = DomainEstablishmentAddress;
