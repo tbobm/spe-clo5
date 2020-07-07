45baa5fea8b1a8d7e902bf573a2d8154dda0ef1f
simoes_t <simoes_t@etna-alternance.net>
2020-07-07 19:32:31 +0200

    routes

--- src/establishment-manager/src/routes/index.ts
+++ src/establishment-manager/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Application } from "../app";
 import express from "express";
 import * as Constants from "../models/utils/Constants";
 import { EstablishmentController } from "../controllers/EstablishmentController";
+import { DomainEstablishment } from "../controllers/types/EstablishmentResource"; 
 
 export function RegisterRoute(application: Application) {
     const app = application.app;
@@ -12,6 +13,34 @@ export function RegisterRoute(application: Application) {
         const list = await controller.getAll();
 
         res.status(200).json(list);
+    });
+
+    app.post("/", async (req: express.Request, res: express.Response) => {
+        const controller : EstablishmentController = container.resolve(Constants.ESTABLISHMENT_CONTROLLER);
+        const ret = await controller.save(new DomainEstablishment(req.body));
+
+        res.status(200).json(ret);
+    });
+
+    app.put("/", async (req: express.Request, res: express.Response) => {
+        const controller : EstablishmentController = container.resolve(Constants.ESTABLISHMENT_CONTROLLER);
+        const ret = await controller.update(new DomainEstablishment(req.body));
+
+        res.status(201).json(ret);
+    });
+
+    app.delete("/:id", async (req: express.Request, res: express.Response) => {
+        const controller : EstablishmentController = container.resolve(Constants.ESTABLISHMENT_CONTROLLER);
+        const ret = await controller.delete(Number(req.params.id))
+
+        res.status(200).json(ret);
+    });
+
+    app.get("/:id", async (req: express.Request, res: express.Response) => {
+        const controller : EstablishmentController = container.resolve(Constants.ESTABLISHMENT_CONTROLLER);
+        const ret = await controller.getOne(Number(req.params.id));
+
+        res.status(200).json(ret);
     });
     
 }
