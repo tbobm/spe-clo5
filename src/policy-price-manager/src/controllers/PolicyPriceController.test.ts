c7cde889c31b139ac5fe8a56252cdb1c8f9cb96a
simoes_t <simoes_t@etna-alternance.net>
2020-07-09 09:36:00 +0200

    Policy Price Manager, mocking data dynamic

--- src/policy-price-manager/src/controllers/PolicyPriceController.test.ts
+++ src/policy-price-manager/src/controllers/PolicyPriceController.test.ts
@@ -104,7 +104,7 @@ describe("Test - Policy Price Controller", () => {
             }
         })
         .catch((error) => {
-            console.log(error.message);        
+            expect(error).to.equal(null);
         });
     });
 
