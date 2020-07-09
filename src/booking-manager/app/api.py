2f344826c7e2fb26400ce975978e8ac5ae6ad22d
simoes_t <simoes_t@etna-alternance.net>
2020-07-09 16:59:25 +0200

    Fix:swagger title

--- src/booking-manager/app/api.py
+++ src/booking-manager/app/api.py
@@ -1,5 +1,5 @@
 from .app import app
 from flask_restx import Api
 
-api = Api(app, version='3.0', title='Sample API',
+api = Api(app, version='3.0', title='Booking Services',
     description='Booking services', doc="/documentation/")
