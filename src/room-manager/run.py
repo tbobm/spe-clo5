e6095df2b195091122ea2ccdfd4042d44ae16f79
simoes_t <simoes_t@etna-alternance.net>
2020-07-09 10:38:48 +0200

    Mocking data dynamic, documentation

--- src/room-manager/run.py
+++ src/room-manager/run.py
@@ -1,5 +1,43 @@
 #!/usr/bin/env python
 from app import app
+from faker import Faker
+import sys
+import json
+import random
 
-if __name__ == '__main__':
+ac  = len(sys.argv)
+fake = Faker()
+
+if (ac > 2 and sys.argv[1] == "mock" and sys.argv[2] == "list"):
+    rooms = [
+        {
+            "id": 1,
+            "name": "Room 01",
+            "room_category_id": 1
+        },
+        {
+            "id": 2,
+            "name": "Room 02",
+            "room_category_id": 1
+        }
+    ]
+    for i in range(len(rooms)):
+        rooms[i]["id"] = random.randint(0, 100)
+        rooms[i]["name"] = fake.company()
+        rooms[i]["room_category_id"] = 1
+    with open('./tests/mock/Rooms.json', 'w') as outfile:
+        json.dump(rooms, outfile, indent=4, sort_keys=True)
+elif (ac > 2 and sys.argv[1] == "mock" and sys.argv[2] == "read"):
+    room = {
+        "id": 1,
+        "name": "Room 01",
+        "room_category_id": 1
+    }
+    room["id"] = random.randint(0, 100)
+    room["name"] = fake.company()
+    room["room_category_id"] = 1
+    with open('./tests/mock/Room.json', 'w') as outfile:
+        json.dump(room, outfile, indent=4, sort_keys=True)
+elif __name__ == '__main__':
     app.run()
+
