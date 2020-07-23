30ecbe65910479e84490850217184e17169358a4
simoes_t <simoes_t@etna-alternance.net>
2020-07-23 11:03:35 +0200

    fix:unit test address

--- src/address-manager/src/controllers/AddressController.test.ts
+++ src/address-manager/src/controllers/AddressController.test.ts
@@ -23,27 +23,11 @@ describe("TEST - ADDRESS CONTROLLER TEST", () => {
         const mockAddress = JSON.parse(fs.readFileSync(`${MOCK_DIRECTORY}/Address.json`, {
             encoding: "utf-8"
         }));
-        sinon.stub(addressRepository, "count").callsFake((param) => {
-            return (Promise.resolve(98));
-        });
-        sinon.stub(addressRepository, "find").callsFake((param) => {
-            const tab = [];
-            for (let i = 0; i < mockAddresses.length; i++){
-                const address: Address = new Address();
-                const mockAddress = mockAddresses[i];
-
-                address.city = mockAddress.city;
-                address.id = mockAddress.id;
-                address.postalCode = mockAddress.postalCode;
-                address.roadNumber = mockAddress.roadNumber;
-                address.road = mockAddress.road;
-                address.country = mockAddress.country;
-                tab.push(address);
-            }
-            return (Promise.resolve(tab));
-        });
-        sinon.stub(addressRepository, "findOne").callsFake((param) => {
-            const address = new Address();
+        const mock = sinon.mock(addressRepository);
+        const tab = [];
+        for (let i = 0; i < mockAddresses.length; i++){
+            const address: Address = new Address();
+            const mockAddress = mockAddresses[i];
 
             address.city = mockAddress.city;
             address.id = mockAddress.id;
@@ -51,8 +35,19 @@ describe("TEST - ADDRESS CONTROLLER TEST", () => {
             address.roadNumber = mockAddress.roadNumber;
             address.road = mockAddress.road;
             address.country = mockAddress.country;
-            return (Promise.resolve(address));
-        });
+            tab.push(address);
+        }
+        const address = new Address();
+
+        address.city = mockAddress.city;
+        address.id = mockAddress.id;
+        address.postalCode = mockAddress.postalCode;
+        address.roadNumber = mockAddress.roadNumber;
+        address.road = mockAddress.road;
+        address.country = mockAddress.country;
+        mock.expects("count").returns(98);
+        mock.expects("find").returns(tab);
+        mock.expects("findOne").withExactArgs(mockAddress.id).returns(address);
     });
 
     after(async () => {
@@ -71,10 +66,9 @@ describe("TEST - ADDRESS CONTROLLER TEST", () => {
         const mockAddresses = JSON.parse(fs.readFileSync(`${MOCK_DIRECTORY}/Addresses.json`, {
             encoding: "utf8"
         }));
- 
-        supertest(application.app)
-        .get("/")
-         .then((response) => {
+
+        try {
+            const response = await supertest(application.app).get("/");
             const body = response.body;
             const addresses = body.data;
 
@@ -85,34 +79,30 @@ describe("TEST - ADDRESS CONTROLLER TEST", () => {
                     expect(address[key]).to.equal(mockAddresses[i][key]);
                 }
             }
-        })
-        .catch((error) => {
+        }
+        catch (error){
             expect(error).to.equal(null);
-        });
+        }
     });
 
     it("check address", async () => {
         const mockAddress = JSON.parse(fs.readFileSync(`${MOCK_DIRECTORY}/Address.json`, {
             encoding: "utf-8"
         }));
-        const mockAddressId = JSON.parse(fs.readFileSync(`${MOCK_DIRECTORY}/AddressId.json`, {
-            encoding: "utf8"
-        }));
 
-        supertest(application.app)
-        .get(`/${mockAddressId.id}`)
-        .expect(200)
-        .then((response) => {
+        try {
+            const response = await supertest(application.app)
+            .get(`/${mockAddress.id}`)
+            .expect(200);
             const body = response.body;
             const address = body.data;
 
             for (let key in address){
                 expect(address[key]).to.equal(mockAddress[key]);
             }
-        })
-        .catch((error) => {
+        }
+        catch(error){
             expect(error).to.equal(null);
-        });
+        }
     });
-
 });
