5bd487809265e18c84de75bafaee6e40935fce10
Theo Massard <theo@memento-solution.com>
2020-07-28 10:53:18 +0200

    Normalize microservices structure
    Move microservices to /src
    Remove .idea directory

--- microservices/service-manager/test/all-is-work.js
+++
@@ -1,9 +0,0 @@
-const chai = require("chai")
-
-describe("All works", () => {
-    it("hello world", () => {
-        const message = "Hello World";
-
-        chai.expect(message).to.equal("Hello World");
-    });
-});
--- microservices/service-manager/test/server-stop.js
+++
@@ -1,8 +0,0 @@
-const chai = require("chai");
-const app = require("../app");
-
-describe("Server stop", () => {
-    it("Stop server", async () => {
-        chai.expect(await app.stop()).to.equal(true);
-    });
-});
