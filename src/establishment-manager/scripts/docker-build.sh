0273671c817fdd90b2d493abc27f641e3655f410
simoes_t <simoes_t@etna-alternance.net>
2020-07-28 18:32:48 +0200

    Docker build target etna registry

--- src/establishment-manager/scripts/docker-build.sh
+++ src/establishment-manager/scripts/docker-build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-rm -rf build
 npm run build
 cp Dockerfile build
 cp .env build
@@ -8,6 +7,6 @@ cp ormconfig.prod.json build/ormconfig.json
 cp package.json build
 cd build
 npm i
-docker build -t establishment-manager .
+docker build -t 172.16.228.42:443/establishment-manager:test-rc .
 cd ..
 rm -rf build
